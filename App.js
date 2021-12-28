880cc469d77b5ca886797789c10f5275a8f23f68
SangHoon <joa3668@gmail.com>
2021-12-28 09:48:24 +0900

    Add: expo init

--- App.js
+++ App.js
@@ -1,20 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import react, { useState } from 'react';
+import { StyleSheet, Text, View, TouchableOpacity, StatusBar, TextInput } from 'react-native';
+import { theme } from './colors';
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
+	const [working, setWorking] = useState(true);
+	const [text, setText] = useState('');
+	const travel = () => setWorking(false);
+	const work = () => setWorking(true);
+	const onChangeText = e => setText(e);
+	return (
+		<View style={styles.container}>
+			<StatusBar style="auto" />
+			<View style={styles.header}>
+				<TouchableOpacity onPress={work}>
+					<Text style={{ ...styles.btnText, color: working ? 'white' : theme.grey }}>Work</Text>
+				</TouchableOpacity>
+				<TouchableOpacity onPress={travel}>
+					<Text style={{ ...styles.btnText, color: working ? theme.grey : 'white' }}>Travel</Text>
+				</TouchableOpacity>
+			</View>
+
+			<TextInput value={text} onChangeText={onChangeText} style={styles.input} placeholder={working ? 'Add a To Do' : 'Where do you want to go?'} />
+		</View>
+	);
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+	container: {
+		flex: 1,
+		backgroundColor: '#000',
+	},
+	header: {
+		justifyContent: 'space-between',
+		flexDirection: 'row',
+		marginTop: 100,
+	},
+	btnText: {
+		fontSize: 38,
+		fontWeight: '600',
+	},
+	input: {
+		backgroundColor: 'white',
+		paddingVertical: 15,
+		paddingHorizontal: 20,
+		borderRadius: 30,
+		marginTop: 20,
+		fontSize: 18,
+	},
 });
